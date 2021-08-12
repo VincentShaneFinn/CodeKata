3d864630d008ab226070f208ce694f1154f4fc1c
VincentShaneFinn <vincent.s.finn@gmail.com>
2021-08-11 22:13:16 -0400

    four requirements for greeting kata

---
+++ CSharpKata/Src/GreetingKata.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpKata.Src
+{
+    public static class GreetingKata
+    {
+        public static string Greet(string name)
+        {
+            if (name == null) name = "my friend";
+            else if (IsAllUpperCase(name)) return $"HELLO {name}!";
+            return $"Hello, { name }.";
+        }
+
+        public static string Greet(string[] names)
+        {
+            var name = names[0] + " and " + names[1];
+            return Greet(name);
+        }
+
+        private static bool IsAllUpperCase(string name)
+        {
+            return name.ToCharArray().All(character => char.IsUpper(character));
+        }
+
+    }
+}
---
+++ CSharpKata/Test/GreetingKataTests.cs
@@ -0,0 +1,61 @@
+﻿using CSharpKata.Src;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpKata.Test
+{
+    //https://github.com/testdouble/contributing-tests/wiki/Greeting-Kata
+
+    [TestFixture]
+    public class GreetingKataTests
+    {
+        private string result;
+
+        [Test]
+        public void Simple_greeting()
+        {
+            Greet("Bob");
+
+            ShouldReturn("Hello, Bob.");
+        }
+
+        [Test]
+        public void Handle_null()
+        {
+            Greet((string)null);
+
+            ShouldReturn("Hello, my friend.");
+        }
+
+        [Test]
+        public void Shout()
+        {
+            Greet("JERRY");
+
+            ShouldReturn("HELLO JERRY!");
+        }
+
+        [Test]
+        public void Two_names()
+        {
+            Greet(new string[] { "Jack", "Jill" });
+
+            ShouldReturn("Hello, Jack and Jill.");
+        }
+
+        private void Greet(string[] names)
+        {
+            result = GreetingKata.Greet(names);
+        }
+
+        private void Greet(string name)
+        {
+            result = GreetingKata.Greet(name);
+        }
+
+        private void ShouldReturn(string expected)
+        {
+            result.Should().Be(expected);
+        }
+    }
+}
