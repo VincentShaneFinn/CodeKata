ef2fafe2bb0e9e38eba8e36c5040fe57b2881bb0
VincentShaneFinn <vincent.s.finn@gmail.com>
2021-08-13 17:29:33 -0400

    refactored to be polymorphic

--- CSharpKata/Src/GreetingKata.cs
+++ CSharpKata/Src/GreetingKata.cs
@@ -4,77 +4,74 @@ namespace CSharpKata.Src
 {
     public static class GreetingKata
     {
-        public static string Greet(string name) => GreetArray(new string[] { name });
+        public static string Greet(string name) => Greet(new string[] { name });
 
-        // use polymorphism on caps and normal instead of the wierd checks
         public static string Greet(string[] names)
         {
             if (names.IsNullOrEmpty()) names = new string[] { "my friend" };
-
-            names = SplitCommSeparatedNames(names);
+            names = SpreadCommaSeparatedNames(names);
+            StandardGreetings standardGreetings = new();
+            ShoutGreetings shoutGreetings = new();
 
             var standardNames = names.Where(word => !IsAllCaps(word)).ToArray();
             var shoutNames = names.Where(word => IsAllCaps(word)).ToArray();
 
-            if (shoutNames.IsNullOrEmpty()) return GreetArray(standardNames);
-            if (standardNames.IsNullOrEmpty()) return GreetArray(shoutNames);
-            return GreetArray(standardNames) + " AND " + GreetArray(shoutNames);
+            var conjuction = !standardNames.IsNullOrEmpty() && !shoutNames.IsNullOrEmpty() ? " AND " : string.Empty;
+            return standardGreetings.Greet(standardNames) + conjuction + shoutGreetings.Greet(shoutNames);
         }
 
-        private static string[] SplitCommSeparatedNames(string[] names)
+        private static string[] SpreadCommaSeparatedNames(string[] names)
         {
             var newNames = new List<string>();
 
-            foreach(var name in names)
-            {
-                var splitNames = name.Split(',');
-                newNames.AddRange(splitNames);
-            }
+            foreach (var name in names) newNames.AddRange(name.Split(','));
 
             return newNames.ToArray();
         }
 
-        private static string GreetOne(string name)
-        {
-            if (IsAllCaps(name)) return $"HELLO {name}!";
-            return $"Hello, { name }.";
-        }
-
-        private static string GreetArray(string[] names)
-        {
-            if (names.Length == 1) return GreetOne(names[0]);
-            if (names.Length == 2) return GreetTwo(names[0], names[1]);
-            return GreetMultiple(names);
-        }
-
-        private static string GreetTwo(string name1, string name2)
-        {
-            var conjunction = " and ";
-            if (IsAllCaps(name1) && IsAllCaps(name2)) conjunction = " AND ";
-            return GreetOne(name1 + conjunction + name2);
-        }
-
-        private static string GreetMultiple(string[] names)
-        {
-            string greeting = "Hello";
-            for (int i = 0; i < names.Length; i++)
-            {
-                bool isLast = i == names.Length - 1;
-                if (isLast) greeting += ", and ";
-                else greeting += ", ";
-                greeting += names[i];
-            }
-
-            if (AreAllCaps(names)) greeting = greeting.ToUpper() + "!";
-            else greeting += ".";
-
-            return greeting;
-        }
-
-        private static bool AreAllCaps(string[] names) => 
-            names.All(word => IsAllCaps(word));
-
         private static bool IsAllCaps(string name) =>
             name.ToCharArray().All(character => char.IsUpper(character) || char.IsWhiteSpace(character));
+
+        private abstract class GreetingsBase
+        {
+            public string Greet(string[] names)
+            {
+                if (names.IsNullOrEmpty()) return string.Empty;
+                if (names.Length == 1) return GreetOne(names[0]);
+                if (names.Length == 2) return GreetTwo(names[0], names[1]);
+                return Finish(GreetMultipleStart(names));
+            }
+
+            protected abstract string GreetOne(string name);
+            protected abstract string GreetTwo(string name1, string name2);
+            protected abstract string Finish(string greeting);
+
+            private static string GreetMultipleStart(string[] names)
+            {
+                string greeting = "Hello";
+                for (int i = 0; i < names.Length; i++)
+                {
+                    bool isLast = i == names.Length - 1;
+                    if (isLast) greeting += ", and ";
+                    else greeting += ", ";
+                    greeting += names[i];
+                }
+                return greeting;
+            }
+        }
+
+        private class StandardGreetings : GreetingsBase
+        {
+            protected override string GreetOne(string name) => $"Hello, { name }.";
+            protected override string GreetTwo(string name1, string name2) => GreetOne(name1 + " and " + name2);
+            protected override string Finish(string greeting) => greeting += ".";
+        }
+
+        private class ShoutGreetings : GreetingsBase
+        {
+            protected override string GreetOne(string name) => $"HELLO {name}!";
+            protected override string GreetTwo(string name1, string name2) => GreetOne(name1 + " AND " + name2);
+            protected override string Finish(string greeting) => greeting.ToUpper() + "!";
+        }
     }
 }
