476061f8062c53686248444b92186c9f03a11832
VincentShaneFinn <vincent.s.finn@gmail.com>
2021-08-22 11:30:15 -0400

    moved files into archive

--- JavaKata/src/main/java/Archive/MaxStockProfit.java
+++ JavaKata/src/main/java/Archive/MaxStockProfit.java
@@ -1,11 +1,11 @@
+package Archive;
+
 import java.util.ArrayList;
 
 public class MaxStockProfit {
     public static int[][] compute(int[] input, int k) {
         var output = new ArrayList<int[]>();
-
-        Integer buy = null;
-        Integer sell = null;
+        Integer buy = null, sell = null;
 
         for(int i = 0; i < input.length; i++) {
             Integer previous = i != 0 ? input[i - 1] : null;
@@ -15,7 +15,6 @@ public class MaxStockProfit {
 
             if(next == null) {
                 if(buy != null) {
-                    //and if sell is not null, make pair, or if current is greater than previous add sell
                     if(sell == null && current > previous) sell = current;
                     if(sell != null) output.add(new int[] {buy, sell});
                 }
--- JavaKata/src/test/java/Archive/MaxStockProfitTests.java
+++ JavaKata/src/test/java/Archive/MaxStockProfitTests.java
@@ -1,3 +1,5 @@
+package Archive;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
