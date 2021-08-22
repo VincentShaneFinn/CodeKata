8e13227bd42684fafa8487588b05e329887bcbfa
VincentShaneFinn <vincent.s.finn@gmail.com>
2021-08-22 11:19:37 -0400

    got class working

--- JavaKata/src/main/java/MaxStockProfit.java
+++ JavaKata/src/main/java/MaxStockProfit.java
@@ -4,32 +4,41 @@ public class MaxStockProfit {
     public static int[][] compute(int[] input, int k) {
         var output = new ArrayList<int[]>();
 
-        int direction = 0;
-        Integer previous = null;
         Integer buy = null;
         Integer sell = null;
 
-        for(int current : input) {
-            int newDirection;
-            if(previous == null) continue;
-            if(current < previous) newDirection = -1;
-            else if (current == previous) newDirection = 0;
-            else newDirection = 1;
+        for(int i = 0; i < input.length; i++) {
+            Integer previous = i != 0 ? input[i - 1] : null;
+            Integer current = input[i];
+            Integer next = i + 1 != input.length ? input[i + 1] : null;
 
-            if(direction == -1 && newDirection == 1) buy = current;
-            if(direction == 1 && newDirection == -1 && buy != null) sell = current;
 
-            direction = newDirection;
-
-            if(buy != null && sell != null) {
-                output.add(new int[]{buy, sell});
-                buy = null;
-                sell = null;
+            if(next == null) {
+                if(buy != null) {
+                    //and if sell is not null, make pair, or if current is greater than previous add sell
+                    if(sell == null && current > previous) sell = current;
+                    if(sell != null) output.add(new int[] {buy, sell});
+                }
+                break;
             }
+
+            if(current < next && buy == null) buy = current;
+            if(previous != null && current > previous && buy != null) sell = current;
+            if(previous != null && next != null) {
+                if(previous < current && current > next) {
+                    if(buy != null && sell != null) {
+                        output.add(new int[] {buy, sell});
+                        sell = null;
+                        buy = null;
+                    }
+                }
+            }
+            System.out.println(buy);
+            System.out.println(sell);
         }
 
-        int[][] outputArray = new int[][] {};
-        output.toArray(outputArray);
+
+        int[][] outputArray = output.toArray(new int[output.size()][]);
         return outputArray;
     }
 }
