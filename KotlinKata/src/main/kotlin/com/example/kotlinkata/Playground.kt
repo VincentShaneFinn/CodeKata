263594c0cb8692713f5d7a6fda255bb7362c7730
VincentShaneFinn <vincent.s.finn@gmail.com>
2022-02-06 12:07:25 -0600

    some small changes

---
+++ KotlinKata/src/main/kotlin/com/example/kotlinkata/Playground.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinkata
+
+class Playground {
+
+}
---
+++ KotlinKata/src/test/kotlin/com/example/kotlinkata/PlaygroundTest.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinkata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+class PlaygroundTest {
+
+}
